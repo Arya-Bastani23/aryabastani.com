58cdb3d8c737bee2966467dc013c6345a0e8e1e4
Arya Bastani <aryabastani@AI.lan>
2023-08-04 13:08:31 -0700

    header part two

---
+++ .cache/page-ssr/routes/component---src-pages-projects-js.js
@@ -0,0 +1,43 @@
+"use strict";
+exports.id = 853;
+exports.ids = [853];
+exports.modules = {
+
+/***/ 9008:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  Z: () => (/* binding */ Layout)
+});
+
+// EXTERNAL MODULE: external "/Users/aryabastani/Documents/Website/node_modules/react/index.js"
+var index_js_ = __webpack_require__(5985);
+var index_js_default = /*#__PURE__*/__webpack_require__.n(index_js_);
+// EXTERNAL MODULE: ./.cache/gatsby-browser-entry.js + 11 modules
+var gatsby_browser_entry = __webpack_require__(7076);
+;// CONCATENATED MODULE: ./src/components/Header.js
+function Header(){return/*#__PURE__*/index_js_default().createElement("nav",null,/*#__PURE__*/index_js_default().createElement("h1",null,"Bastani"),/*#__PURE__*/index_js_default().createElement("div",{className:"links"},/*#__PURE__*/index_js_default().createElement(gatsby_browser_entry.Link,{to:"/about"},"About"),/*#__PURE__*/index_js_default().createElement(gatsby_browser_entry.Link,{to:"/blog"},"Blog"),/*#__PURE__*/index_js_default().createElement(gatsby_browser_entry.Link,{to:"/projects"},"Projects")));}
+;// CONCATENATED MODULE: ./src/components/Layout.js
+function Layout({children}){return/*#__PURE__*/index_js_default().createElement("div",{className:"layout"},/*#__PURE__*/index_js_default().createElement(Header,null),/*#__PURE__*/index_js_default().createElement("div",{className:"content"},children),/*#__PURE__*/index_js_default().createElement("footer",null,/*#__PURE__*/index_js_default().createElement("p",null,"Designed and built by Arya Bastani. In whatever town I'm in right now.")));}
+
+/***/ }),
+
+/***/ 7219:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Home)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5985);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9008);
+function Home(){return/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_components_Layout__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z,null,/*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("div",null,"This the Projects page"));}
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-projects-js.js.map
