dd7401ed4a021eefa7a1f55d8d252e416fa948e4
Arya Bastani <aryabastani@AI.lan>
2023-08-25 10:24:01 -0700

    updated to infile css editing for index

--- src/fragments.js
+++ src/fragments.js
@@ -12,6 +12,26 @@ export const siteMetadata = graphql`
   }
 `;
 
+export const hero = graphql`
+  fragment Hero on Query {
+    allMdx(filter: { frontmatter: { title: { eq: "Hero" } } }) {
+      nodes {
+        frontmatter {
+          thumb {
+            childImageSharp {
+              gatsbyImageData(
+                width: 800
+                placeholder: BLURRED
+                transformOptions: { cropFocus: ENTROPY }
+              )
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const postElements = graphql`
   fragment PostElements on Mdx {
     frontmatter {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,45 @@
 import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
-import { box } from "../styles/home.module.css";
+import styled from "styled-components";
+// import { box } from "../styles/home.module.css";
 
-export default function Home() {
-  const Box = ({ children }) => (
-    <section className={box}>
-      {children}
+// .box {
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   grid-gap: 40px;
+//   align-items: center;
+// }
+// .box h2 {
+//   font-size: 4em;
+// }
+// .box h3 {
+//   font-size: 3em;
+//   font-weight: 400;
+//   margin-bottom: 20px;
+// }
+
+const Container = styled.div(
+  ({ theme }) => `
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 40px;
+  align-items: center;
+
+  .h2 {
+    font-size: 4em;
+  }
+
+  .h3 {
+    font-size: 3em;
+    font-weight: 400;
+    margin-bottom: 20px;
+  }
+`
+);
+
+const Home = () => {
+  return (
+    <Container>
       <div>
         <h3>
           Hello, I'm Arya Bastani. I created this site to highlight things that
@@ -19,7 +53,44 @@ export default function Home() {
         placeholder="blurred"
         layout="fixed"
       />
-    </section>
+    </Container>
   );
-  return <Box></Box>;
-}
+};
+
+export default Home;
+
+// Export page query
+export const query = graphql`
+  query Projects {
+    ...Hero
+  }
+`;
+
+// export default function Home() {
+//   const Box = ({ children }) => (
+//     <section className={box}>
+//       {children}
+// <div>
+//   <h3>
+//     Hello, I'm Arya Bastani. I created this site to highlight things that
+//     I think are cool.
+//   </h3>
+//   <p>I graduated from Berkeley before.</p>
+// </div>
+// <StaticImage
+//   src="../images/profile.jpg"
+//   alt="An Arya"
+//   placeholder="blurred"
+//   layout="fixed"
+// />
+//     </section>
+//   );
+//   return <Box></Box>;
+// }
+
+// // Export page query
+// export const query = graphql`
+//   query Projects {
+//     ...Hero
+//   }
+// `;
