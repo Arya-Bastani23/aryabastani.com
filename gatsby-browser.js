596b884ce334fa4fbd650eae7acb9f1082f09bfb
Arya Bastani <aryabastani@AI.lan>
2023-08-30 14:11:42 -0700

    added cow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,14 @@ import "sanitize.css";
 
 export { default as wrapPageElement } from "./wrapPageElement";
 export { default as wrapRootElement } from "./wrapRootElement";
+
+export const onInitialClientRender = () => {
+  const loadTimer = 0;
+  const animTimer = loadTimer + 200;
+  setTimeout(function () {
+    document.getElementById("___loader").style.opacity = "0";
+  }, loadTimer);
+  setTimeout(function () {
+    document.getElementById("___loader").style.display = "none";
+  }, animTimer);
+};
