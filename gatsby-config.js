74c3c80cd64468298f16fb261a0ef1a9171f1eda
Arya Bastani <aryabastani@AI.lan>
2023-08-30 13:32:45 -0700

    no more theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,6 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         stack_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/cow.png`,
       },
